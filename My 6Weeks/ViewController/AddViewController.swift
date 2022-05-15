9e048c0e822555e90cd3e8bdc5fdb89cfe7aecfa
broMac <yhs4705@naver.com>
2022-05-15 21:39:54 +0900

    스토리보드 생성 및 연결 중

--- My 6Weeks/ViewController/AddViewController.swift
+++ My 6Weeks/ViewController/AddViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class AddViewController: UIViewController {
-
+    @IBOutlet weak var diaryImageView: UIImageView!
+    @IBOutlet weak var diaryTitleTextField: UITextField!
+    @IBOutlet weak var diaryDateButton: UIButton!
+    
+    @IBOutlet weak var diaryContentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
